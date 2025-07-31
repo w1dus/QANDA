f49a7e419e5e4295cfc306050a17f4d2df49923e
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-07-31 15:44:29 +0900

    Qanda

--- script/script.js
+++ script/script.js
@@ -1,7 +1,25 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+  slideMenuToggleHandler();
+  yotubeBackground_player();
 })
 
+const yotubeBackground_player = () => {
+  $('[data-vbg]').youtube_background(); // 실행
+}
 
+const slideMenuToggleHandler = () => {
+  $('header .menu').click(function(){
+    $('.close-menu-bg').addClass('show');
+    $('.right-slide-menu').addClass('show');
+  })
+  $('.right-slide-menu .btn-wrap .close-btn').click(function(){
+    $('.close-menu-bg').removeClass('show');
+    $('.right-slide-menu').removeClass('show');
+  })
+  $('.close-menu-bg').click(function(){
+    $('.close-menu-bg').removeClass('show');
+    $('.right-slide-menu').removeClass('show');
+  })
+}
